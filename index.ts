dd3aa3eee9cda970820cab8d434619e01e324441
Hirwa Rene <hirwarene6@gmail.com>
2025-04-01 15:23:58 +0200

    ...

--- index.ts
+++ index.ts
@@ -6,5 +6,17 @@ class Car {
     this.speed += amount;
     console.log(`Accelerating to ${this.speed} km/h`);
   }
+}
 
+class ElectricCar extends Car {
+  private batteryLevel: number = 100;
+
+  constructor(brand: string, private batteryCapacity: number) {
+    super(brand);
+  }
+
+  charge(amount: number): void {
+    this.batteryLevel = Math.min(this.batteryLevel + amount, this.batteryCapacity);
+    console.log(`Charging to ${this.batteryLevel}%`);
+  }
 }
