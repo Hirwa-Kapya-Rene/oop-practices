ab243c1f8932685c4e2eea7cfb37ff46634c498c
Hirwa Rene <hirwarene6@gmail.com>
2025-04-01 15:24:22 +0200

    ...

--- index.ts
+++ index.ts
@@ -8,15 +8,3 @@ class Car {
   }
 }
 
-class ElectricCar extends Car {
-  private batteryLevel: number = 100;
-
-  constructor(brand: string, private batteryCapacity: number) {
-    super(brand);
-  }
-
-  charge(amount: number): void {
-    this.batteryLevel = Math.min(this.batteryLevel + amount, this.batteryCapacity);
-    console.log(`Charging to ${this.batteryLevel}%`);
-  }
-}
