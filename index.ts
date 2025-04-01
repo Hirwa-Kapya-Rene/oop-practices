64bc147870a338485cf87b5475ade18144bea85a
Hirwa Rene <hirwarene6@gmail.com>
2025-04-01 15:52:51 +0200

    ....

--- index.ts
+++ index.ts
@@ -1,5 +1,7 @@
 class Car {
   private speed: number = 0;
+  private fuel: number = 100;
+  name: string = "Tesla";
   constructor(public brand:string) {}
 
   accelerate(amount: number): void {
@@ -8,3 +10,5 @@ class Car {
   }
 }
 
+const myCar = new Car("Tesla");
+myCar.accelerate(50);
