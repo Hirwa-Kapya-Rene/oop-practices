9f538cfba3be9511e673eb97b12907fccb29e2e3
Hirwa Rene <hirwarene6@gmail.com>
2025-04-03 10:48:23 +0200

    commented the code

--- index.js
+++ index.js
@@ -1,21 +1,21 @@
-var Car = /** @class */ (function () {
-    function Car(brand) {
-        this.brand = brand;
-        this.speed = 0;
-    }
-    Car.prototype.accelerate = function (amount) {
-        this.speed += amount;
-        console.log("Accelerating to ".concat(this.speed, " km/h"));
-    };
-    return Car;
-}());
+// var Car = /** @class */ (function () {
+//     function Car(brand) {
+//         this.brand = brand;
+//         this.speed = 0;
+//     }
+//     Car.prototype.accelerate = function (amount) {
+//         this.speed += amount;
+//         console.log("Accelerating to ".concat(this.speed, " km/h"));
+//     };
+//     return Car;
+// }());
 
 
-const http = require('http');
+// const http = require('http');
 
-const server = http.createServer((req, res) => {
-    res.writeHead(200, { 'Content-Type': 'text/plain' });
-    res.end('Bonjour Newton Renesto, Node.js fonctionne !');
-});
+// const server = http.createServer((req, res) => {
+//     res.writeHead(200, { 'Content-Type': 'text/plain' });
+//     res.end('Bonjour Newton Renesto, Node.js fonctionne !');
+// });
 
-server.listen(3000, () => console.log('Serveur sur http://localhost:3000'));
+// server.listen(3000, () => console.log('Serveur sur http://localhost:3000'));
