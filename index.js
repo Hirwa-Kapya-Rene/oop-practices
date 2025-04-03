b097ea1a41b9b1444eea2a27687c81ec1928b979
Hirwa Rene <hirwarene6@gmail.com>
2025-04-03 09:25:11 +0200

    added var car

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
-// var Car = /** @class */ (function () {
-//     function Car(brand) {
-//         this.brand = brand;
-//         this.speed = 0;
-//     }
-//     Car.prototype.accelerate = function (amount) {
-//         this.speed += amount;
-//         console.log("Accelerating to ".concat(this.speed, " km/h"));
-//     };
-//     return Car;
-// }());
+var Car = /** @class */ (function () {
+    function Car(brand) {
+        this.brand = brand;
+        this.speed = 0;
+    // }
+    // Car.prototype.accelerate = function (amount) {
+    //     this.speed += amount;
+    //     console.log("Accelerating to ".concat(this.speed, " km/h"));
+    // };
+    // return Car;
+}());
 
 
 const http = require('http');
