373170dd5238bada79d9bc7ecf5d0e9153d81f7b
Hirwa Rene <hirwarene6@gmail.com>
2025-04-04 19:41:02 +0200

    created htt const

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ var Car = /** @class */ (function () {
 }());
 
 
-// const http = require('http');
+const http = require('http');
 
 // const server = http.createServer((req, res) => {
 //     res.writeHead(200, { 'Content-Type': 'text/plain' });
