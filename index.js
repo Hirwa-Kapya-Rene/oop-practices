d92ae3e042b160f0e69103811be2d327a6179a19
Hirwa Rene <hirwarene6@gmail.com>
2025-04-04 19:36:14 +0200

    added function car

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 var Car = /** @class */ (function () {
-//     function Car(brand) {
-//         this.brand = brand;
-//         this.speed = 0;
-//     }
+    function Car(brand) {
+        this.brand = brand;
+        this.speed = 0;
+    }
 //     Car.prototype.accelerate = function (amount) {
 //         this.speed += amount;
 //         console.log("Accelerating to ".concat(this.speed, " km/h"));
